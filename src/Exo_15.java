3b08ec9f5a503aa0f42f63c8c335bcc30f082fe4
Julien <julien.loeuilleux62@gmail.com>
2023-07-24 10:01:41 +0200

    commit

--- src/Exo_15.java
+++ src/Exo_15.java
@@ -1,17 +1,57 @@
+
+
 import java.util.Scanner;
 
 public class Exo_15 {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in, "UTF-8");
-//        System.out.println("Saisir un nombre : ");
-//        int nombre = scanner.nextInt();
-//        int diviseur = 0;
-//        System.out.println(22%2);
-//        for(int i = 1; i <= nombre; i++){
-//            if(nombre%i ==0 || (((nombre/i)-0.5)%i) == 0)
-//                double res = (nombre/i)-0.5;
-//            System.out.println(i);
-//            System.out.println(( 45.00/ 2.00)-0.5)%2);
+        Scanner scanner = new Scanner(System.in, "UTF-8");
+        System.out.println("Saisir un nombre : ");
+        int nombre = scanner.nextInt();
+        int i = 0;
+        int j = 1;
+        int k = 1;
+        int somme = 1;
+        int count = 0;
+        int s = 0;
+        while (nombre != i) {
+            somme = k;
+            s = k;
+            while (nombre != somme) {
+                if (j == nombre ){
+                    break;
+                }
+                somme = (j + 1) + somme;
+                j++;
+                count++;
+
+                if(somme == nombre && count != nombre){
+                    somme = -1;
+                    System.out.print(nombre + " = ");
+                    while(  somme <nombre){
+                        somme = (s + 1) + somme;
+                        if(somme < nombre){
+                        System.out.print(s + " + ");}
+
+
+                        if (somme >= nombre){
+                            System.out.println(s);
+                            break;
+                        }
+                        s++;
+                    }
+
+
+                }
+
+            }
+
+            count = 0;
+            i++;
+            k++;
+            j= k;
+
         }
     }
 
+
+}
